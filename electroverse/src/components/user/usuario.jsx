66759eb83b3ea26fa56b661e53b7c7d2b7afa99e
Martin Santos <martin.santos.curadelli@gmail.com>
2023-11-22 23:31:07 -0300

    conexion con el back hecha, falta cargar usuarios en el back

--- electroverse/src/components/user/usuario.jsx
+++ electroverse/src/components/user/usuario.jsx
@@ -1,13 +1,26 @@
 import './usuario.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import axios from 'axios'
 
 export default function Login() {
+    const [mail, nombre] = useState("")
+    const [password, contraseña] = useState("")
+    const manejarLogin = async (e) => {
+        e.preventDefault()
+        try{
+            await axios.post("http://localhost:8080/login", {mail, password})
+            console.log("inicio de seision exitoso")
+        }
+        catch(error){
+            console.log("No se ha podido iniciar sesion")
+        }
+    }
+
     const [ showPassword, setShowPassword ] = useState(false);
 
     const changePasswordVisibility1 = (e) => {
         e.preventDefault();
-        console.log("ENTRO AL CHANGE")
         setShowPassword(!showPassword);
     };
     return (
@@ -18,19 +31,19 @@ export default function Login() {
                         <h2 class="login-title">Login</h2>
                         <div class="inputbox">
                             <ion-icon name="mail-outline"></ion-icon>
-                            <input type="email" name="email" required/>
+                            <input type="email" name="email"  value={mail} onChange={(e) => nombre(e.target.value)} required/>
                             <label for="">Email</label>
                         </div>
                         <div class="inputbox">
                             <ion-icon name="lock-closed-outline" onClick={changePasswordVisibility1} />
-                            <input name="password" type="password"  required/>
+                            <input name="password" type="password" value={password} onChange={(e) => contraseña(e.target.value)}  required/>
                             <label for="password">Password</label>
                         </div>
                         <div class="forget">
                             <label for=""><input type="checkbox"/>Remember Me <a href="#">Forget Password</a></label>
                             
                         </div>
-                        <button class="boton-login">Log in</button>
+                        <button class="boton-login" onClick={manejarLogin}>Log in</button>
                         <div class="register">
                             <p>No eres miembro? <Link to='/register' >Regístrate ahora.</Link></p>
                         </div>
